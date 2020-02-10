b735bb7d8a359341e96a9bd3827d82cf3829dc92
Ramon Sanchez <songwright66@gmail.com>
2020-02-10 14:01:00 -0800

    Imported articleContent into ArticlePage

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
+  const article = articleContent.find(article => article.name === name);
+
   return (
     <>
-      <h1>This is the {name} article</h1>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
     </>
   );
 }
