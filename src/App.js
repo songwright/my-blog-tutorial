b701786297059ed5edc0de876410a0790ab3c868
Ramon Sanchez <songwright66@gmail.com>
2020-02-10 13:10:21 -0800

    Imported AboutPage, ArticlesList, ArticlePage into App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesList from "./pages/ArticlesList";
+import ArticlePage from "./pages/ArticlePage";
 import './App.css';
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/articles-list" component={ArticlesList} />
+        <Route path="/article" component={ArticlePage} />
       </div>
     </Router>
   );
