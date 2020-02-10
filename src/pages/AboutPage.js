1afcdff59538333c804034e78b85e1b9b16d9e32
Ramon Sanchez <songwright66@gmail.com>
2020-02-10 13:05:22 -0800

    Created AboutPage

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <>
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga esse nihil aperiam mollitia nostrum totam consectetur error blanditiis ex, iusto architecto ipsam officia nam commodi odio, nisi voluptas nulla quisquam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga esse nihil aperiam mollitia nostrum totam consectetur error blanditiis ex, iusto architecto ipsam officia nam commodi odio, nisi voluptas nulla quisquam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga esse nihil aperiam mollitia nostrum totam consectetur error blanditiis ex, iusto architecto ipsam officia nam commodi odio, nisi voluptas nulla quisquam.
+      </p>
+    </>
+  );
+}
+ 
+export default AboutPage;
