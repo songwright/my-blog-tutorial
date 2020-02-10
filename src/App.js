12fb4c1d3561120e94a29a2476370c09162cfc05
Ramon Sanchez <songwright66@gmail.com>
2020-02-10 13:40:07 -0800

    Imported NavBar into App

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlesList from "./pages/ArticlesList";
 import ArticlePage from "./pages/ArticlePage";
+import NavBar from "./NavBar";
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <div className="App" id="page-body">
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/articles-list" component={ArticlesList} />
-        <Route path="/article" component={ArticlePage} />
+        <Route path="/article/:name" component={ArticlePage} />
       </div>
     </Router>
   );
