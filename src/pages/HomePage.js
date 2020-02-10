9e59d6865169123a8cb3082b1885a3a12b0dda0b
Ramon Sanchez <songwright66@gmail.com>
2020-02-10 11:18:46 -0800

    Created HomePage.js in pages folder

---
+++ src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Welcome to Ramon's Blog</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga esse nihil aperiam mollitia nostrum totam consectetur error blanditiis ex, iusto architecto ipsam officia nam commodi odio, nisi voluptas nulla quisquam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga esse nihil aperiam mollitia nostrum totam consectetur error blanditiis ex, iusto architecto ipsam officia nam commodi odio, nisi voluptas nulla quisquam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga esse nihil aperiam mollitia nostrum totam consectetur error blanditiis ex, iusto architecto ipsam officia nam commodi odio, nisi voluptas nulla quisquam.
+      </p>
+    </>
+  );
+}
+ 
+export default HomePage;
