b8feda98670f4016653c768356cad37c0b79680b
Ramon Sanchez <songwright66@gmail.com>
2020-02-10 16:31:16 -0800

    Imported articleContent into ArticlesList

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import articleContent from "./article-content";
 
 const ArticlesList = () => {
   return (
     <>
       <h1>Articles</h1>
+      {articleContent.map((article, key) => (
+        <Link key={key} to={`/article/${article.name}`}>
+          <h3>{article.title}</h3>
+        </Link>
+        
+      ))}
     </>
   );
 }
